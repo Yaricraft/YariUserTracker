cd6f6b7d9deff8dcf47fbe2cd26f309964d35ebf
Timothy Fike <tafike@gmail.com>
2014-04-29 01:02:06 -0400

    Added configs and disk commands.
    The "/yut save" command will write data to disk and is called on server
    exit by default.

--- eclipse/YariUserTracker/src/main/java/com/yaricraft/YariUserTracker/YariUserTracker.java
+++ eclipse/YariUserTracker/src/main/java/com/yaricraft/YariUserTracker/YariUserTracker.java
@@ -5,10 +5,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -27,11 +35,22 @@ import com.google.api.client.util.store.DataStoreFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
+//import com.google.api.services.drive.model.File;
 
 public final class YariUserTracker extends JavaPlugin {
 	
 	public Map<String, Integer> playerPoints = new HashMap<String, Integer>();
+	
+	File actionsFile;
+    File configFile;
+    File usersFile;
+    File historyFile;
+
+    FileConfiguration actions;
+    FileConfiguration config;
+    FileConfiguration users;
+    FileConfiguration history;
+    
 	
 	private enum eCommand {
 		NONE, ADD, REMOVE, PUNISH
@@ -39,12 +58,104 @@ public final class YariUserTracker extends JavaPlugin {
 	
     @Override
     public void onEnable() {
-    	getLogger().info("Loading config...");
+    	getLogger().info("Loading configs...");
+        configFile = new File(getDataFolder(), "config.yml");
+        usersFile = new File(getDataFolder(), "users.yml");
+        actionsFile = new File(getDataFolder(), "actions.yml");
+        historyFile = new File(getDataFolder(), "history.yml");
+        
+        try {
+            firstRun();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        config = new YamlConfiguration();
+        actions = new YamlConfiguration();
+        users = new YamlConfiguration();
+        history = new YamlConfiguration();
+        
+        loadYamls();
+        
+        getLogger().info("Points max: "+config.getString("repmax"));
     }
  
+    // We need to save when the server exits.
     @Override
     public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
+    	saveYamls();
+    }
+    
+    private void firstRun() throws Exception {
+        if(!configFile.exists()){                        // checks if the yaml does not exists
+            configFile.getParentFile().mkdirs();         // creates the /plugins/<pluginName>/ directory if not found
+            copy(getResource("config.yml"), configFile); // copies the yaml from your jar to the folder /plugin/<pluginName>
+        }
+        if(!actionsFile.exists()){
+            actionsFile.getParentFile().mkdirs();
+            copy(getResource("groups.yml"), actionsFile);
+        }
+        if(!usersFile.exists()){
+            usersFile.getParentFile().mkdirs();
+            copy(getResource("users.yml"), usersFile);
+        }
+        if(!historyFile.exists()){
+            historyFile.getParentFile().mkdirs();
+            copy(getResource("history.yml"), historyFile);
+        }
+    }
+    
+    private void copy(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void loadYamls() {
+        try {
+            config.load(configFile); //loads the contents of the File to its FileConfiguration
+            actions.load(actionsFile);
+            users.load(usersFile);
+            history.load(historyFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        for(String i : users.getKeys(false))
+        {
+        	if(playerPoints.containsKey(i))
+        	{
+        		getLogger().info("Player "+i+" was already loaded, ignoring...");
+        	}else{
+        		playerPoints.put(i, users.getInt(i));
+        	}
+        }
+    }
+    
+    public void saveYamls() {
+    	
+    	for(String i : playerPoints.keySet())
+    	{
+    		users.set(i, playerPoints.get(i));
+    	}
+    	
+        try {
+            config.save(configFile); //saves the FileConfiguration to its File
+            actions.save(actionsFile);
+            users.save(usersFile);
+            history.save(historyFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     // The only command is /yut. We don't need to check other commands.
@@ -52,14 +163,13 @@ public final class YariUserTracker extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	
     	// See how the command was received. (Console or Player)
-    	
     	Player player = (Player) sender;
     	
     	// Read the command. Arguments can be in any order.
-    	
     	eCommand opCommand = eCommand.NONE;
     	int opNumber = 0;
     	List<String> opPlayers = new ArrayList<String>();
+    	String strDiskCommand = "";
     	
     	for(int i = 0; i < args.length; i++)
     	{
@@ -95,6 +205,28 @@ public final class YariUserTracker extends JavaPlugin {
     			player.sendMessage("Error: No valid punishment found.");
     			return false;
     		}
+    		if(args[i].equalsIgnoreCase("save"))
+    		{
+    			if(strDiskCommand.equals(""))
+    			{
+    				strDiskCommand = "save";
+    				continue;
+    			}else{
+    				player.sendMessage("Error: Can't use multiple disk commands.");
+        			return true;
+    			}
+    		}
+    		if(args[i].equalsIgnoreCase("load"))
+    		{
+    			if(strDiskCommand.equals(""))
+    			{
+    				strDiskCommand = "load";
+    				continue;
+    			}else{
+    				player.sendMessage("Error: Can't use multiple disk commands.");
+        			return true;
+    			}
+    		}
     		try
     		{
     			opNumber = Integer.parseInt(args[i]);
@@ -118,8 +250,11 @@ public final class YariUserTracker extends JavaPlugin {
 	    		player.sendMessage("Error: No players were found for command "+opCommand.toString());
 	    		return true;
     		}else{
-    			player.sendMessage("TODO: Add help.");
-    			return true;
+    			if(strDiskCommand.equals(""))
+    			{
+    				player.sendMessage("TODO: Add help.");
+    				return true;
+    			}
     		}
     	}
     	
@@ -137,11 +272,23 @@ public final class YariUserTracker extends JavaPlugin {
     	
     	// Done reading command, now process.
     	
+    	// Load
+    	if(strDiskCommand.equals("load"))
+    	{
+    		loadYamls();
+    		player.sendMessage("YariUserTracker loaded from disk.");
+    	}
+    	
+    	// Change rep.
     	String playername;
     	int playerpoints;
     	for(int i = 0; i < opPlayers.size(); i++)
     	{
     		playername = opPlayers.get(i);
+    		if(playername.equals("save") || playername.equals("load") )
+    		{
+    			
+    		}
     		if(playerPoints.containsKey(playername))
     		{
     			playerpoints = playerPoints.get(playername);
@@ -183,6 +330,13 @@ public final class YariUserTracker extends JavaPlugin {
 		    	}
 				continue;
 	    	}
+    	}
+    	
+    	// Save
+    	if(strDiskCommand.equals("save"))
+    	{
+    		saveYamls();
+    		player.sendMessage("YariUserTracker saved to disk.");
     	}
 
     	// If the command worked return true.
